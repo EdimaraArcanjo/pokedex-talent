ba527a6188d70af1a018425dcd1cfe9d68b69150
Bárbara Souza <babisouza0704@gmail.com>
2023-07-21 12:31:48 -0300

    atualizando

--- pokedex-talent/src/CardModal.jsx
+++ pokedex-talent/src/CardModal.jsx
@@ -4,14 +4,47 @@ import './CardModal.css'
 
 function colorByType(type) {
   switch (type) {
+    case "normal":
+      return "#a6a877";
     case "grass":
-      return "green";
+      return "#77c850";
+    case "fire":
+      return "#ee7f30";
+    case "water":
+      return "#678fee";
+    case "eletric":
+      return "#f7cf2e";
+    case "ice":
+      return "#98d5d7";
+    case "ground":
+      return "#dfbf69";
+    case "flying":
+      return "#a98ff0";
     case "poison":
-      return "purple";
+      return "#a98ff0";
+    case 'fighting':
+      return '#bf3029';
+    case 'psychic':
+      return '#f65687';
+    case 'dark':
+      return '#725847';
+    case 'rock':
+      return '#b8a137';
+    case 'bug':
+      return '#a8b720';
+    case 'ghost':
+      return '#6e5896';
+    case 'steel':
+      return '#b9b7cf';
+    case 'dragon':
+      return '#6f38f6';
+    case 'fairy':
+      return '#f9aec7';
     default:
       return "#fff";
   }
 }
+
 const CardModal = ({ selectedPokemon, closeModal }) => {
   return (
     <>
